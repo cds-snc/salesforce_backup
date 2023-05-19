cd9c8bc6d270207deeac7134f4707297361c9e9f
CalvinRodo <calvin.rodo@cds-snc.ca>
2023-05-19 16:07:58 -0400

    fix: enable logging

--- terragrunt/s3.tf
+++ terragrunt/s3.tf
@@ -4,9 +4,15 @@ module "cds_salesforce_backups_bucket" {
   bucket_name        = "cds-salesforce-backups"
   billing_tag_value  = var.billing_code
   critical_tag_value = "true"
-  versioning = {
-    "enabled" = true
+
+  logging = {
+    target_bucket = module.cds_salesforce_backups_logs_bucket.s3_bucket_id
   }
+
+  versioning = {
+    enabled = true
+  }
+
 }
 
 module "cds_salesforce_backups_logs_bucket" {
